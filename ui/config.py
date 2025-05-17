9e70891ec2fecee9239696be2c6933656d34383e
charlesdwright <charlesdwright@gmai.com>
2025-05-16 21:55:57 -0400

    updated backend URL

--- ui/config.py
+++ ui/config.py
@@ -3,7 +3,7 @@
 # Configuration for the UI application
 CONFIG = {
     #"backend_url": "http://localhost:8000/chat",  # URL of the backend service
-    "backen_url": "https://1759-2603-9000-9101-5690-d534-4127-12f-6774.ngrok-free.app",
+    "backen_url": "https://9f2b-2603-9000-9101-5690-d534-4127-12f-6774.ngrok",
     "theme": "light",  # UI theme choice (could be light or dark)
     "title": "My Awesome Chatbot UI",  # Title for the Streamlit app
     "default_input": "Hello, how can I assist you today?",  # Default text input for chatbot
